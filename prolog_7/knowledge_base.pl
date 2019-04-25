b2f1f20bba8193f8b89bea0503d404dae2e14afd
majakubo <40472381+majakubo@users.noreply.github.com>
2019-04-25 19:31:21 +0200

    Update knowledge_base.pl

--- prolog_7/knowledge_base.pl
+++ prolog_7/knowledge_base.pl
@@ -1,6 +1,3 @@
-% FACTS
-
- 
 %board([ [w  ,w  ,w  ,w  ,w  ,w  ,w  ,w  ,w  ],
 %        [w  ,a  ,a  ,c  ,c  ,d  ,d  ,j  ,w  ],
 %        [w  ,a  ,a  ,c  ,c  ,d  ,d  ,j  ,w  ],
@@ -11,37 +8,37 @@
 %        [w  ,w  ,w  ,w  ,w  ,w  ,w  ,w  ,w  ]]).
 
 
-board([[w, w, w, w, w],   %
-       [w, a, x, x, w],   %a
-       [w, x, x, x, w],   %b
+board([[w, w, w, w, w],   
+       [w, a, b, x, w],   
+       [w, x, x, x, w],   
        [w, w, w, w, w]]).
 
-depth(2).
-depth(4).
-depth(8).
-depth(16).
-depth(32).
-depth(64).
-depth(128).
-depth(256).
-depth(512).
+steps(2).
+steps(4).
+steps(8).
+steps(16).
+steps(32).
 
-block(b).
-block(c).
-block(d).
-block(e).
+
+
+
+%block(b).
+%block(c).
+%block(d).
+%block(e).
 block(a).
-direction(right).
+
 direction(down).
-direction(left).
+direction(right).
 direction(up).
+direction(left).
 
 height(a, X):- X is 1.
 height(b, X):- X is 2.
-height(c, X):- X is 3.
-height(d, X):- X is 4.
-height(e, X):- X is 5.
-height(f, X):- X is 6.
+%height(c, X):- X is 3.
+%height(d, X):- X is 4.
+%height(e, X):- X is 5.
+%height(f, X):- X is 6.
 	
 width(1, 1).
 width(2, 2).
@@ -51,19 +48,35 @@ width(5, 5).
 width(6, 6).
 width(7, 7).
 
+xydiff(D, Y, X):-
+    D = 'left',
+    Y is 0,
+    X is -1.
+xydiff(D, Y, X):-
+    D = 'right',
+    Y is 0,
+    X is 1.
+xydiff(D, Y, X):-
+    D = 'up',
+    Y is -1,
+    X is 0.	
+xydiff(D, Y, X):-
+    D = 'down',
+    Y is 1,
+    X is 0.
+
+
 % PREDICATS
 
 %------------------
-	max_depth(Board, MaxDepth):-
-        append(Board, FlatBoard),
-     	length(FlatBoard, MaxDepth).
-        
-
-        
+	max_steps(MaxSteps):-
+        board(Board),
+    	find_all_objects(Board, Objects, 'x'),
+     	length(Objects, MaxSteps). 
 %------------------
 
+
 %------------------
-	% n character in characters
     n_character(N, Chars, Char):-
         string_chars(Chars, Str),
         nth0(N, Str, Char).	
@@ -101,13 +114,42 @@ width(7, 7).
 
 
 %------------------
-	loop_through_sequence([]).
-	loop_through_sequence([_|T]):-
-        loop_through_sequence(T).
+    change_in_list(Index, Elem, [_|T], [Elem|T]):-
+        Index = 0,
+    	!.
+	
+    change_in_list(Index, Elem, [H|T], [H|Out]):-
+        Index1 is Index - 1,
+        change_in_list(Index1, Elem, T, Out).
 %------------------
 
 
 %------------------
+	insert_into_two_dimensional_array(Y, X, Object, Board, NewBoard):-
+        nth0(Y, Board, ColFrom),
+        change_in_list(X, Object, ColFrom, NewColFrom),
+        change_in_list(Y, NewColFrom, Board, NewBoard).
+%------------------
+
+
+%------------------
+    unpack_one_element_list([H|_], H).
+
+
+    unpack_two_element_list([H|T], H, B):-
+        unpack_one_element_list(T, B).
+
+
+	unpack_three_element_list([H|T], H, B, C):-
+        unpack_two_element_list(T, B, C).
+%------------------
+
+
+%------------------
+	find_all_objects(Board, Objects, Character):-
+        append(Board, L),
+    	find_all_objects_unwrap(L, [], Objects, Character).
+	
 	find_all_objects_unwrap([], ObjectsTemp, ObjectsTemp, _).
     
     find_all_objects_unwrap([H|T], ObjectsTemp, Objects, Character):-
@@ -118,9 +160,6 @@ width(7, 7).
     find_all_objects_unwrap([_|T], ObjectsTemp, Objects, Character):-
 		find_all_objects_unwrap(T, ObjectsTemp, Objects, Character), !.
 
-    find_all_objects(Board, Objects, Character):-
-        append(Board, L),
-    	find_all_objects_unwrap(L, [], Objects, Character).
 %------------------	
 
   
@@ -135,8 +174,7 @@ width(7, 7).
          find_all_cords_of_block_unwrap(NewBoard, T, ListOfCordsTemp1, ListOfCords), !.
     
 	find_all_cords_of_block(Board, Character, ListOfCords):-
-        n_character(0, Character, ID),
-        find_all_objects(Board, Objects, ID),
+        find_all_objects(Board, Objects, Character),
         find_all_cords_of_block_unwrap(Board, Objects, [], ListOfCords).
 %------------------
 
@@ -168,77 +206,12 @@ width(7, 7).
 
 
 %------------------
-	one_step(Board, NewBoard):-
-       move_block(Board, a, right, NewBoard), !.
-
-	one_step(Board, NewBoard):-
-       direction(Dir),
-       block(BlockCharacter),
-       move_block(Board, BlockCharacter, Dir, NewBoard).
-%------------------
-
-	
-%------------------    
-	move_block(Board, BlockCharacter, Direction, NewBoard):-
-        can_block_move(Board, BlockCharacter, Direction),
-        find_all_cords_of_block(Board, BlockCharacter, ListOfCords),
-    	replace_cords_with_character(Board, Board1, ListOfCords, x),
-        get_new_cords(ListOfCords, ListOfCords1, Direction),
-        replace_cords_with_character(Board1, NewBoard, ListOfCords1, BlockCharacter).
-%------------------
-
-
-%------------------
-	make_move(Board, Board, Depth, MaxDepth, Final_Y, Final_X, []):-
-        not(Depth > MaxDepth),
-        find_corner(Board, Y, X, a),
-        Y is Final_Y,
-        X is Final_X, 
-    	!.
-	make_move(Board, NewBoard, Depth, MaxDepth,  Final_Y, Final_X, MOVES):-
-        not(Depth > MaxDepth),
-        one_step(Board, BoardT),
-        IncDepth is Depth + 1,
-    	make_move(BoardT, NewBoard, IncDepth, MaxDepth, Final_Y, Final_X, MOVEST),
-        MOVES = [BoardT | MOVEST],
-    	!.
-%------------------
-
-
-
-%------------------	
-	przem(StartY, StartX, EndY, EndX):-
-    	width(StartX, SX),
-        height(StartY, SY),
-    	width(EndX, EX),
-        height(EndY, EY),
-        board(B),
-        max_depth(B, MaxDepth),
-        przem_unwrap(B, SY, SX, EY, EX, 0, MaxDepth).
-	
-	przem_unwrap(_, StartY, StartX, EndY, EndX, _, _):-
-        StartX = EndX,
-        StartY = EndY.
-
-	przem_unwrap(Board, StartY, StartX, EndY, EndX, Depth, MaxDepth):-
-        position(Board, StartY, StartX,  Char),
-        direction(Dir),
-        move_block(Board, Char, Dir, NewBoard),
-        position(NewBoard, TempY, TempX, Char),
-    	NewDepth = Depth + 1,
-        przem_unwrap(NewBoard, TempY, TempX, EndY, EndX, NewDepth, MaxDepth). 
-%------------------
-	    
-
-%------------------
     can_single_cell_move(Board, Y, X, Ydiff, Xdiff):-
         position(Board, Y, X, BlockCharacter),
-        n_character(0, BlockCharacter, FirstBlockCharacter),
         Y1 is Y + Ydiff,
         X1 is X + Xdiff,
         position(Board, Y1, X1, CellCharacter),
-        n_character(0, CellCharacter, FirstCellCharacter),
-        (FirstCellCharacter = FirstBlockCharacter ; FirstCellCharacter = 'x').
+        (CellCharacter = BlockCharacter ; CellCharacter = 'x').
 %------------------
 
 
@@ -252,61 +225,89 @@ width(7, 7).
 
 
 %------------------
-    can_block_move(Board, BlockCorner, Direction):-
+    can_block_move(Board, Direction, ListOfCords):-
         xydiff(Direction, Ydiff, Xdiff),
-        find_all_cords_of_block(Board, BlockCorner, ListOfCords),
         can_every_cell_move(Board, ListOfCords, Ydiff, Xdiff).
 %------------------
 
 
-%------------------
-        
-	xydiff(D, Y, X):-
-        D = 'left',
-        Y is 0,
-        X is -1.
-	xydiff(D, Y, X):-
-        D = 'right',
-        Y is 0,
-        X is 1.
-	xydiff(D, Y, X):-
-        D = 'up',
-        Y is -1,
-        X is 0.	
-	xydiff(D, Y, X):-
-        D = 'down',
-        Y is 1,
-        X is 0.
+
+%------------------    
+	move_block(Board, BlockCharacter, Direction, NewBoard):-
+    	find_all_cords_of_block(Board, BlockCharacter, ListOfCords),
+        can_block_move(Board, Direction, ListOfCords),
+    	replace_cords_with_character(Board, Board1, ListOfCords, x),
+        get_new_cords(ListOfCords, ListOfCords1, Direction),
+        replace_cords_with_character(Board1, NewBoard, ListOfCords1, BlockCharacter).
 %------------------
 
 
 %------------------
-    change_in_list(Index, Elem, [_|T], [Elem|T]):-
-        Index = 0,
-    	!.
+	%best move for the game
+	one_step(Board, NewBoard):-
+       move_block(Board, a, right, NewBoard), !.
 	
-    change_in_list(Index, Elem, [H|T], [H|Out]):-
-        Index1 is Index - 1,
-        change_in_list(Index1, Elem, T, Out).
+	%random  move
+	one_step(Board, NewBoard):-
+       direction(Dir),
+       block(BlockCharacter),
+       move_block(Board, BlockCharacter, Dir, NewBoard).
+%------------------
+	
+
+%------------------
+	make_move(Board, Board, Steps, MaxSteps, Final_Y, Final_X, []):-
+        not(Steps > MaxSteps),
+        find_corner(Board, Y, X, a),
+        Y is Final_Y,
+        X is Final_X, 
+    	!.
+	make_move(Board, NewBoard, Steps, MaxSteps,  Final_Y, Final_X, MOVES):-
+        not(Steps > MaxSteps),
+        one_step(Board, BoardT),
+        IncSteps is Steps + 1,
+    	make_move(BoardT, NewBoard, IncSteps, MaxSteps, Final_Y, Final_X, MOVEST),
+        MOVES = [BoardT | MOVEST],
+    	!.
 %------------------
 
 
+%------------------ 
+
+	rozwiaz(FinalY, FinalX, FinalBoard, MOVES):-
+    	height(FinalY, Y),
+    	width(FinalX, X),
+        board(B),
+    	steps(MaxSteps),
+    	make_move(B, FinalBoard, 0, MaxSteps, Y, X, MOVES), !.
+     	
 %------------------
-	insert_into_two_dimensional_array(Y, X, Object, Board, NewBoard):-
-        nth0(Y, Board, ColFrom),
-        change_in_list(X, Object, ColFrom, NewColFrom),
-        change_in_list(Y, NewColFrom, Board, NewBoard).
+
+
+
+%------------------	
+	przem(StartY, StartX, EndY, EndX):-
+    	width(StartX, SX),
+        height(StartY, SY),
+    	width(EndX, EX),
+        height(EndY, EY),
+        board(B),
+        max_steps(MaxSteps),
+        przem_unwrap(B, SY, SX, EY, EX, 0, MaxSteps).
+	
+	przem_unwrap(_, StartY, StartX, EndY, EndX, Steps, MaxSteps):-
+        not(Steps > MaxSteps),
+    	StartX = EndX,
+        StartY = EndY, !.
+
+	przem_unwrap(Board, StartY, StartX, EndY, EndX, Steps, MaxSteps):-
+        not(Steps > MaxSteps),
+    	position(Board, StartY, StartX,  Char),
+        direction(Dir),
+        move_block(Board, Char, Dir, NewBoard),
+        position(NewBoard, TempY, TempX, Char),
+    	IncSteps = Steps + 1,
+        przem_unwrap(NewBoard, TempY, TempX, EndY, EndX, IncSteps, MaxSteps), !. 
 %------------------
+	    
 
-
-%------------------
-    unpack_one_element_list([H|_], H).
-
-
-    unpack_two_element_list([H|T], H, B):-
-        unpack_one_element_list(T, B).
-
-
-	unpack_three_element_list([H|T], H, B, C):-
-        unpack_two_element_list(T, B, C).
-%------------------
